2b85b02d061174295fe0100608b9d018e5a4a652
Joseph Hancock <joe.hancock95@gmail.com>
2026-03-31 14:17:09 -0400

    improved styling & added logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import Link from "next/link";
+import Image from "next/image";
 import { ReactNode } from "react";
+
 
 export const metadata = {
   title: "Zealthy Exercise",
@@ -13,10 +15,19 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body>
         <main>
           <div className="space-between" style={{ marginBottom: 24 }}>
-            <div>
+            {/* <div>
               <div className="">Zealthy Exercise</div>
               <h1 style={{ marginTop: 12 }}>Mini-EMR & Patient Portal</h1>
-            </div>
+            </div> */}
+            <Link href="/" className="inline-flex items-center">
+              <Image
+                src="/zealthyLogo.svg"
+                alt="Portal logo"
+                width={200}
+                height={55}
+                priority
+              />
+            </Link>
             <nav className="row">
               <Link href="/">Patient Login</Link>
               <Link href="/portal">Portal</Link>
